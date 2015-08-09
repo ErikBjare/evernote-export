b25982e5d7317993740b0018bb333c5e2bbab941
Erik BjÃ¤reholt <erik.bjareholt@gmail.com>
2015-08-09 07:17:29 +0200

    Works with quirks

--- do-it.sh
+++ do-it.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # VARS
-max=10
+# Change to your wishes
+MAX=1000
 
 # SETUP / BOOTSTRAP / PREPARE
 mkdir -p logs
 
 # Find all notes to dump
-geeknote find --count $max
+geeknote find --count $MAX
 
-for i in {1..10}; do
+START=1
+END=$MAX
+for ((i=$START; i<=$END; i++)); do
     echo $i
     geeknote show $i > logs/$i.md
     echo "One down."
